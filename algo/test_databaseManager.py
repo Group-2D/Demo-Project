22d4c24dfd87ac6df109a8b9d60d3c5b4397804b
up2109872 <up2109872@myport.ac.uk>
2024-04-04 21:47:24 +0100

    Update test_databaseManager.py

--- algo/test_databaseManager.py
+++ algo/test_databaseManager.py
@@ -13,6 +13,8 @@ class TestDbManager(unittest.TestCase):
 
     #create test functions 
     def test_selectAll(self):
+
+        #test case 1 - valid test case for 'lecturer' table
         expected = [
             (1, 'Taylor', 'Swift', None),
             (2, 'Adam', 'Levine', None),
@@ -20,12 +22,39 @@ class TestDbManager(unittest.TestCase):
             (4, 'Katy', 'Perry', None)
         ]
         
-        
         self.session.selectAll('lecturer')
         result = self.session.dbCursor.fetchall()
-        print(expected, '\n', result)
         self.assertEqual((result), expected)
 
+        #test case 2 - invalid test case due to misspelling
+        expected = False
+        self.assertEqual((self.session.selectAll('lecturers')), expected)
+        
+        #test case 3 - invalid test case due to type error (None type)
+        expected = False
+        self.assertEqual((self.session.selectAll(2)), expected)
+
+        #test case 4 - valid test case for 'building' table
+        expected = [
+            ('Angelesea'),
+            ('Liongate'),
+            ('Park'),
+            ('Richmond')
+        ]
+        self.session.selectAll('building')
+        result = self.session.dbCursor.fetchall()
+        self.assertEqual((result), expected)
+
+        #test case 5 - valid test case for 'Building' - handles uppercase misspelling correctly
+        self.session.selectAll('Building')
+        result = self.session.dbCursor.fetchall()
+        self.assertEqual((result), expected)
+
+        #test case 6 - invalid test case due to type error (Int type)
+        expected = False
+        self.assertEqual((self.session.selectAll(10)), expected)
+        #test case 7 - invalid test case due to type erro (Bool type)
+        self.assertEqual((self.session.selectAll(True)), expected)
     def test_selectOnConditon(self):
         pass
 
