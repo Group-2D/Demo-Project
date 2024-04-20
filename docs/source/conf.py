e13e1ae754c6a1490a0e8bd23744a5802f96b27b
jackbthomas1 <148047591+jackbthomas1@users.noreply.github.com>
2024-04-20 07:13:36 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SETAP Team 2D - University Timetable Generator'
+copyright = '2024, Setap Team 2D'
+author = 'up2111450, up....'
 
 release = '0.1'
 version = '0.1.0'
