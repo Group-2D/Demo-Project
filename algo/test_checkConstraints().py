d903995d09822bb5e29874cd37e01864af7edc9b
bfararjeh <bahafararjeh@hotmail.com>
2024-04-29 13:19:17 +0100

    test algo updates

--- algo/test_checkConstraints().py
+++ algo/test_checkConstraints().py
@@ -7,43 +7,6 @@ from algorithm_with_db_functionality import checkConstraints, Module
 
 class TestCheckConstraints(unittest.TestCase):
 
-    # Test the function is operating with standard input parameters
-    # def test_validInput(self):
-
-    #     dummy_modInfo = MagicMock()
-    #     dummy_modInfo.modName = 'Architecture & Operating Systems'
-        
-    #     self.assertTrue(checkConstraints(module_info = dummy_modInfo,
-    #                                      classType = "lec",
-    #                                      randPotentialRoom = 6,
-    #                                      studentsUnassigned= 200,
-    #                                      room = ['RLT2', 160, (4,), None],
-    #                                      professorsList = [
-	# 		('Miss', 'Taylor', 'Swift', 'Architecture & Operating Systems', 
-    #          '000000000000000001000000001000000001000000001'),
-    #         ('Mr','Adam', 'Levine', 'Networks',
-    #          '000010010010000100001000001100000000000010000'),
-    #         ('Mr', 'Lewis', 'Capaldi', 'Programming', 
-    #          '000000000000000000000000000000000000000000000'),
-    #         ('Miss', 'Katy', 'Perry', 'Programming', 
-    #          '000000000000000000000000000000000000000000000'),
-    #         ('Dr', 'John', 'Smith', 
-    #          'Architecture & Operating Systems, Programming, Comp Tutorial 4', 
-    #          '000000100000000100000000100000000100000000100'),
-    #         ('Dr', 'Lisa', 'Franklin', 'Core Computing Concepts', 
-    #          '000000000000000000000000000000000000000000000'),
-    #         ('Dr', 'Herbert', 'Jones', 'Core Computing Concepts', 
-    #          '000000000000000000000000000000000000000000000'),
-    #         ('Dr', 'Richard', 'Johnson', 'Database Systems Development', 
-    #          '000010000000000010000000000000010000000000000'),
-    #         ('Dr', 'Hugh', 'Piper', 'Programming', 
-    #          '000000000000000000000000000000000000000000000'),
-    #         ('Dr', 'Javier', 'Rodriguez', 'Networks', 
-    #          '000000000000000000000000000000000000000000000'),
-    #         ('Dr', 'Kathlyn', 'Ferguson', 'Networks', 
-    #          '000000000000001000000000000100000000000000001')]))
-
-
     dummy_modInfo = MagicMock()
     dummy_modInfo.modName = 'Architecture & Operating Systems'
 
@@ -75,7 +38,7 @@ class TestCheckConstraints(unittest.TestCase):
     @parameterized.expand([
         ("valid", dummy_modInfo, "lec", 6, 200, ['RLT2', 160, (4,), None], profsList),
         ("invalid classType", dummy_modInfo, "peepee", 6, 200, ['RLT2', 160, (4,), None], profsList),
-        ("invalid randPotentialRoom", dummy_modInfo, "lec", 1239, 200, ['RLT2', 160, (4,), None], profsList)
+        ("invalid randPotentialRoom", dummy_modInfo, "lec", 1239, 200, ['RLT2', 160, (4,), None], profsList),
         ("invalid room", dummy_modInfo, "lec", 6, 200, ['blank', 0, (82,), None], profsList),
         ("invalid professorsList", dummy_modInfo, "lec", 6, 200, ['RLT2', 160, (4,), None], ["Mister","Invalid"]),
     ])
